58271aec9a4286fc30091bccbdcc11881285002c
Victor Prado <prado.victor1005@gmail.com>
2023-09-04 01:45:56 -0300

    criando user repository

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,56 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { hash } from 'bcryptjs';
+import { UserRepository } from 'src/shared/database/repositories/users.repositories';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly UsersRepo: UserRepository) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto;
+
+    const emailTaken = await this.UsersRepo.findUnique({
+      where: { email },
+      select: { id: true },
+    });
+
+    if (emailTaken) {
+      throw new ConflictException('This email is already in use.');
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    const user = await this.UsersRepo.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        categories: {
+          createMany: {
+            data: [
+              { name: 'Salário', icon: 'salary', type: 'INCOME' },
+              { name: 'Freelance', icon: 'freelance', type: 'INCOME' },
+              { name: 'Outro', icon: 'other', type: 'INCOME' },
+              // Expense
+              { name: 'Casa', icon: 'home', type: 'EXPENSE' },
+              { name: 'Alimentação', icon: 'food', type: 'EXPENSE' },
+              { name: 'Educação', icon: 'education', type: 'EXPENSE' },
+              { name: 'Lazer', icon: 'fun', type: 'EXPENSE' },
+              { name: 'Mercado', icon: 'grocery', type: 'EXPENSE' },
+              { name: 'Roupas', icon: 'clothes', type: 'EXPENSE' },
+              { name: 'Transporte', icon: 'transport', type: 'EXPENSE' },
+              { name: 'Viagem', icon: 'travel', type: 'EXPENSE' },
+              { name: 'Outro', icon: 'other', type: 'EXPENSE' },
+            ],
+          },
+        },
+      },
+    });
+
+    return {
+      name: user.name,
+      email: user.email,
+    };
+  }
+}
---
+++ src/shared/database/repositories/users.repositories.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { type Prisma } from '@prisma/client';
+
+import { PrismaService } from 'src/shared/database/prisma.service';
+
+@Injectable()
+export class UserRepository {
+  constructor(private readonly prismaService: PrismaService) {}
+
+  create(createDto: Prisma.UserCreateArgs) {
+    return this.prismaService.user.create(createDto);
+  }
+
+  findUnique(findUniqueDto: Prisma.UserFindUniqueArgs) {
+    return this.prismaService.user.findUnique(findUniqueDto);
+  }
+}
